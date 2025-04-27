e533aefccbf55f4e8b8625b9f7c5feea90e92791
Rahul Kumar <rahulkumarsingh2508@gmail.com>
2025-04-27 13:50:19 +0530

    Add files via upload

---
+++ src/main/java/com/ioc/coupling/IOCExample.java
@@ -0,0 +1,26 @@
+package com.ioc.coupling;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class IOCExample {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationIoCLooseCouplingExample.xml");
+
+        //UserDataProvider databaseProvider = new UserDatabaseProvider();
+        //UserManager userManagerWithDB = new UserManager(databaseProvider);
+        UserManager userManagerWithDB = (UserManager) context.getBean("userManagerWithUserDataProvider");
+        System.out.println(userManagerWithDB.getUserInfo());
+
+        //UserDataProvider webServiceProvider = new WebServiceDataProvider();
+        //UserManager userManagerWithWs = new UserManager(webServiceProvider);
+        UserManager userManagerWithWS = (UserManager) context.getBean("userManagerWithWebServiceProvider");
+        System.out.println(userManagerWithWS.getUserInfo());
+
+        //UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
+        //UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
+        UserManager userManagerWithNewDB = (UserManager) context.getBean("userManagerWithNewDatabaseProvider");
+        System.out.println(userManagerWithNewDB.getUserInfo());
+    }
+}
